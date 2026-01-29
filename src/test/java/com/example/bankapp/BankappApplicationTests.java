22ec1db9fc6287759c669832cbf945abaab18ee6
adkab99 <114829549+adkab99@users.noreply.github.com>
2026-01-29 16:08:30 -0600

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -1,3 +1,15 @@
-@Test
-@Disabled
-void contextLoads() {}
+package com.example.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BankappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
